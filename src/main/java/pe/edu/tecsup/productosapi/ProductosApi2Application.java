dd40b732ae7103206d78f74fd41339f9b649fe68
Jaime <jgomezm@tecsup.edu.pe>
2021-05-13 19:49:48 -0500

    First version

---
+++ src/main/java/pe/edu/tecsup/productosapi/ProductosApi2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.productosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductosApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductosApi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/tecsup/productosapi/ProductosApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.productosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
